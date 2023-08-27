ef62303e48adf377b59bf20de362447c8aa0ddfd
dushyantxo <141238622+dushyantxo@users.noreply.github.com>
2023-08-27 14:25:32 +0530

    Add files via upload

--- random python/codes/gvvoython.py
+++ random python/codes/gvvoython.py
@@ -322,12 +322,13 @@ x_BC = line_gen(B,C)
 x_CA = line_gen(C,A)
 x_BE = line_gen(B,E)
 x_CF = line_gen(C,F)
-
+x_AD = line_gen(A,D)
 
 #Plotting all lines
 plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
 plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
 plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+plt.plot(x_AD[0,:],x_AD[1,:],label='$AD$')
 plt.plot(x_BE[0,:],x_BE[1,:],label='$BE$')
 plt.plot(x_CF[0,:],x_CF[1,:],label='$CF$')
 
@@ -455,6 +456,58 @@ print(f"{norm_vec_AD}[x-A]=0" )
 H = line_intersect(norm_vec(B,E),E,norm_vec(C,F),F)
 print('orthocentre ',H)
 #plotting
+x_AB = line_gen(A,B)	
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+x_AD = line_gen(A,alt_foot(A,B,C))
+x_AE = line_gen(A,alt_foot(B,A,C))
+x_BE = line_gen(B,alt_foot(B,A,C))
+x_CF = line_gen(C,alt_foot(C,A,B))
+x_AF = line_gen(A,alt_foot(C,A,B))
+x_CH = line_gen(C,H)
+x_CF = line_gen(C,F)
+
+
+x_BD = line_gen(B,D)
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+plt.plot(x_AD[0,:],x_AD[1,:],label='$AD$')
+plt.plot(x_BE[0,:],x_BE[1,:],label='$BE_1$')
+plt.plot(x_AE[0,:],x_AE[1,:],linestyle = 'dashed',label='$AE_1$')
+plt.plot(x_BD[0,:],x_BD[1,:],linestyle = 'dashed',label='$BD_1$')
+plt.plot(x_CF[0,:],x_CF[1,:])
+plt.plot(x_AF[0,:],x_AF[1,:],linestyle = 'dashed',label='$AF_1$')
+
+
+#Labeling the coordinates
+A = A.reshape(-1,1)
+B = B.reshape(-1,1)
+C = C.reshape(-1,1)
+D = D.reshape(-1,1)
+E = E.reshape(-1,1)
+F = F.reshape(-1,1)
+H = H.reshape(-1,1)
+tri_coords = np.block([[A,B,C,D,E,F]])
+#tri_coords = np.vstack((A,B,C,alt_foot(A,B,C),alt_foot(B,A,C),alt_foot(C,A,B),H)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','C','D_1','E_1','F_1']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the poi3.5nt to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+plt.show()
+plt.savefig("main5.png",bbox_inches='tight')
+#plotting
 #Generating all lines
 x_AB = line_gen(A,B)	
 x_BC = line_gen(B,C)
@@ -467,7 +520,7 @@ x_AF = line_gen(A,alt_foot(C,A,B))
 x_CH = line_gen(C,H)
 x_BH = line_gen(B,H)
 x_AH = line_gen(A,H)
-
+x_BD = line_gen(B,D)
 #Plotting all lines
 plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
 plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
@@ -475,6 +528,7 @@ plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
 plt.plot(x_AD[0,:],x_AD[1,:],label='$AD$')
 plt.plot(x_BE[0,:],x_BE[1,:],label='$BE_1$')
 plt.plot(x_AE[0,:],x_AE[1,:],linestyle = 'dashed',label='$AE_1$')
+plt.plot(x_BD[0,:],x_BD[1,:],linestyle = 'dashed',label='$BD_1$')
 plt.plot(x_CF[0,:],x_CF[1,:],label='$CF_1$')
 plt.plot(x_AF[0,:],x_AF[1,:],linestyle = 'dashed',label='$AF_1$')
 plt.plot(x_CH[0,:],x_CH[1,:],label='$CH$')
